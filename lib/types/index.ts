045c52b054cebae5973246fa29792a2f3dfa79d1
Neil Merton <neilmerton@gmail.com>
2026-03-08 19:02:34 +0000

    refactor: change type definitions from type to interface for better extensibility

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,41 +1,41 @@
-export type Instance = {
+export interface Instance {
   id: string;
   name: string;
   email: string;
 };
 
-export type Job = {
+interface BaseEntity {
   id: string;
   instance_id: string;
+  date: string;
+  company: string;
+  contact_name: string | null;
+  status: string;
+  last_update_date: string | null;
+}
+
+export interface Job extends BaseEntity {
   date_applied: string;
   role: string;
   description: string | null;
   job_type: string;
   source: string;
   link: string | null;
-  company: string;
-  contact_name: string | null;
   contact_email: string | null;
   contact_mobile: string | null;
-  status: string;
-  last_update_date: string | null;
 };
 
-export type Connection = {
-  id: string;
-  instance_id: string;
+export interface Connection extends BaseEntity {
   date_requested: string;
-  company: string;
-  contact_name: string | null;
   contact_linkedin_url: string | null;
   contact_mobile: string | null;
-  status: string;
-  last_update_date: string | null;
 };
 
-export type Update = {
+export type UpdateType = "job" | "connection";
+
+export interface Update {
   id: string;
-  type: "job" | "connection";
+  type: UpdateType;
   parent_id: string;
   date: string;
   description: string;
